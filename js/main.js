6f247b018a66944ed6c70f96c080a663749ac25a
Kiri <kirimartin@Kiris-MBP.home>
2017-05-09 18:23:25 +0100

    added first hotspot
    with JS for text box pop up

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('it works');
+$(document).ready(function(){
+	console.log('it works');
+
+	//when i click on a hotspot the cooresponding text box appears
+	//when i click on the hotspot again text box disapears
+
+	var $button = $('.button');
+	var $textbox = $('.text-box');
+
+	$button.on('click', function(){
+		$textbox.slideToggle( 300 );
+	});
+
+
+
+
+//make all hotspots appear when you hover over the photo
+//when 1 hotspot is clicked all others disapear
+
+
+
+	});
