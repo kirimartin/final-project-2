66bbad02137fe15d6d276269bbecf3882f735469
Kiri <kirimartin@Kiris-MBP.home>
2017-05-18 11:22:17 +0100

    New file
    2 hotspots working and positioned correctly

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
+	console.log('it works');
 	
+	$('.tooltip-1').tooltipster();
+
+	$('.tooltip-1').tooltipster({
+		position:'right',
+		trigger:'click',
+		animation:'grow'
+	})
+
 	$('.tooltip').tooltipster();
-	$('.tooltip-right').tooltipster
 
-
-	$('.tooltip-right').tooltipster({
+	$('.tooltip').tooltipster({
 		position:'right',
 		trigger:'click',
 		animation:'grow'
 	})
 	// why doesnt this work?
-
 
 
 });
